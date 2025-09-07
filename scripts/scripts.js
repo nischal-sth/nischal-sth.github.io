7f5334d1542bfe29b89c9deb6ba10af8a4002666
nischal-sth <nischalshrestha2@mail.weber.edu>
2025-09-07 13:06:50 -0700

    added images and some code

---
+++ scripts/scripts.js
@@ -0,0 +1,15 @@
+function boring( ){
+    document.getElementById('main').style="background-color:white;"
+    document.getElementById('modes').innerHTML="boring....."
+}
+function defult( ){
+    document.getElementById('main').style="background-color: rgb(143, 223, 93)"
+    document.getElementById('modes').innerHTML="defult"
+}
+function batman( ){
+    
+    document.getElementById('main').style="background-color:black;"
+    document.getElementById('modes').innerHTML="batman"
+    
+    
+}
