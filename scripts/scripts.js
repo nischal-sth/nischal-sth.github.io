826f623b4a9f2335169c0872af1dc997566af531
nischal-sth <nischalshrestha2@mail.weber.edu>
2025-09-14 12:37:25 -0700

    ADDED CALCULATIONS

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,3 +13,40 @@ function batman( ){
     
     
 }
+function perimeter1( ) {
+
+	let a1 = document.getElementById('a').value;
+	let b1 = document.getElementById('b').value;
+    let c1 = document.getElementById('c').value;
+
+                
+	const result = parseFloat (a1) + parseFloat(b1) + parseFloat(c1); 
+				
+	document.getElementById('result').innerHTML = result;
+}
+
+function area( ) {
+
+	
+	let base = document.getElementById('base').value;
+    let hight = document.getElementById('hight').value;
+
+                
+	const result = 0.5 * parseFloat(base) * parseFloat(hight)
+				
+	document.getElementById('result2').innerHTML = result;
+
+
+}
+
+
+function volume( ) {
+
+	let L = document.getElementById('L').value;
+	let W = document.getElementById('W').value;
+    let H = document.getElementById('H').value;
+
+                
+	const result = parseFloat (L) * parseFloat(W) * parseFloat(H); 
+				
+	document.getElementById('result3').innerHTML = result;}
